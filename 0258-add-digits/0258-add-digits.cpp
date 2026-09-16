a062f49aa7ea8f29fa753c5beb02d05f66d18a71
Aryan Shashwat <aryan.main27@gmail.com>
2026-09-17 00:55:47 +0530

    Time: 0 ms (100%), Space: 8.7 MB (45.43%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -3,9 +3,14 @@ public:
     int addDigits(int num) {
         
         while(num/10){
-            num = num/10 + num%10;
+            int sum = num/10 + num%10;
+            num = sum;
         }
 
         return num;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
