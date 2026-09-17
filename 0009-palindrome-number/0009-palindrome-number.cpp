d681cbd311dd135189f599dda9f5412756212b68
Aryan Shashwat <aryan.main27@gmail.com>
2026-09-17 20:53:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (64.9%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        // if(x < 0) return false;
+
+        // string ans = to_string(x);
+
+        // int start = 0,end = ans.size()-1;
+        // while(start <= end){
+        //     if(ans[start] == ans[end]){
+        //         start++;
+        //         end--;
+        //     }else{
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
         if(x < 0) return false;
-
-        string ans = to_string(x);
-
-        int start = 0,end = ans.size()-1;
-        while(start <= end){
-            if(ans[start] == ans[end]){
-                start++;
-                end--;
-            }else{
-                return false;
-            }
+        int num = x;
+        long long xcopy = 0;
+        while(num){
+            int digit = num%10;
+            num = num/10;
+            xcopy = xcopy*10 + digit;
         }
-
-        return true;
+        
+        return (int)xcopy == x;
     }
 };
 
