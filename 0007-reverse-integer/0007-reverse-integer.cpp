f74990eb3a80bf81f73ec4773ca9ff07a261ed51
Aryan Shashwat <aryan.main27@gmail.com>
2026-09-17 20:41:56 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.29%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    int reverse(int x){
-        // Current Complexity:
-        // Time: O(log10(x)) - We iterate through the digits of the number.
-        // Space: O(1) - Constant space used.
-        // This is the optimal complexity for this problem.
-
-        int ans = 0;
-
-        while(x){
-            // Critical Bug: Integer Overflow. 
-            // When reversing a number like 1534236469, 'ans * 10' will exceed the 32-bit integer limit.
-            // LeetCode constraints specify that the input is a signed 32-bit integer, 
-            // and you must return 0 if the reversed integer overflows.
-            if((ans > INT_MAX/10) || (ans < INT_MIN/10)) 
-                return 0;
-            ans = ans*10 + x%10;
-            x = x/10;
+    int reverse(int num) {
+        long long x = 0;
+        while(num){
+            int digit = num%10;
+            num/=10;
+            if(x*10 >= (-1)*pow(2,31) && x*10 <= (pow(2,31) -1)) 
+            x = x*10 + digit;
+            else 
+            return 0;
         }
-        return ans;
+
+        return (int)x;
     }
 };
-
-// COACHING HINTS:
-// 1. To handle overflow, you can use a 'long' variable for 'ans' and check if it exceeds INT_MAX/INT_MIN.
-// 2. Alternatively, check if 'ans > INT_MAX/10' before multiplying by 10.
-// 3. Be careful with INT_MIN; consider using the modulo operator on the negative number directly 
-//    instead of converting to positive.
-// 4. If you get stuck, check the "Video Solutions" section in the Solutions tab on the left pane!
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
