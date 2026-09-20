328a12962f9c089260783aa721fece25f87a29e3
Aryan Shashwat <aryan.main27@gmail.com>
2026-09-20 11:06:28 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704718-height-of-heap-GFG/704718-height-of-heap.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int heapHeight(int n, int arr[]) {
+        // code here
+        if(n == 1) return 1;
+        return floor(log2(n));
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
